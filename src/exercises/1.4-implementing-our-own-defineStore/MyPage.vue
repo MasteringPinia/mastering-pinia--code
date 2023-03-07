028d493c7e25f9d22de2e8d5cfbeb9d3cf8b01a4
Eduardo San Martin Morote <posva13@gmail.com>
2023-03-07 18:05:27 +0100

    add ex

---
+++ src/exercises/1.4-implementing-our-own-defineStore/MyPage.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { useCountStore } from './store'
+
+const { double, n } = useCountStore()
+</script>
+
+<template>
+  <p>Count: {{ n }}. {{ n }} x 2 = {{ double }}</p>
+  <button @click="n++">Increment ({{ n }})</button>
+</template>
---
+++ src/exercises/1.4-implementing-our-own-defineStore/store.ts
@@ -0,0 +1,12 @@
+import { ref, computed } from 'vue'
+import { defineStore } from './my-pinia'
+
+export const useCountStore = defineStore(() => {
+  const n = ref(0)
+  const increment = (amount = 1) => {
+    n.value += amount
+  }
+  const double = computed(() => n.value * 2)
+
+  return { n, double, increment }
+})
