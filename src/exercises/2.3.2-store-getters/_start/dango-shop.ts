ae7ec616eede42ede30dec4f533d20064b401373
Eduardo San Martin Morote <posva13@gmail.com>
2023-05-02 15:32:51 +0200

    final

--- src/exercises/2.3.2-store-getters/_start/dango-shop.ts
+++ src/exercises/2.3.2-store-getters/_start/dango-shop.ts
@@ -3,3 +3,6 @@ import { defineStore } from 'pinia'
 export const useDangoShop = defineStore('2.3.1 store-state', {
   state: () => ({ amount: 0 }),
 })
+
+// Do not change this value, you will need it
+const DANGO_PRICE = 350
