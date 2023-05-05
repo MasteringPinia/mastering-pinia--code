2f51b0c4815748b4a945cc12963a368d6526feca
Eduardo San Martin Morote <posva13@gmail.com>
2023-05-05 15:20:57 +0200

    start from getters

---
+++ src/exercises/2.5.1-store-actions/_start/index.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { useDangoShop } from './dango-shop'
+
+const dangoShop = useDangoShop()
+</script>
+
+<template>
+  <h1>Dango Shop 🍡</h1>
+
+  <button data-test="btn-add" class="mx-2" :disabled="dangoShop.amount >= 100" @click="dangoShop.amount++">
+    More 🍡
+  </button>
+  <button data-test="btn-remove" :disabled="dangoShop.amount < 1" @click="dangoShop.amount--">Remove one 🍡</button>
+
+  <DangoShopMessages232>
+    <!-- You will only need to display new things here -->
+    You are ordering {{ dangoShop.amount }} dango{{ dangoShop.amount > 1 ? 's' : '' }}. That would be a total of
+    <span>¥350</span>.
+    <template v-if="dangoShop.hasPriceDiscount">
+      You are saving ¥{{ dangoShop.savedMoney }} with our special offer.
+    </template>
+  </DangoShopMessages232>
+
+  <!-- You won't need to change any of the classes of these elements -->
+  <section data-test="dangos" class="grid lg:grid-cols-5 grid-cols-3 pt-[100px] pl-[40px] pr-[200px]">
+    <div v-for="i in dangoShop.amount" :key="i" class="w-1/3 h-[50px] rotate-[262deg] origin-center">
+      <DangoStick232 />
+    </div>
+  </section>
+</template>
