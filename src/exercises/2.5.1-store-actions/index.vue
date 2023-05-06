5a779de9182bf23a8a3c4e5db07e8e71dd7d72ca
Eduardo San Martin Morote <posva13@gmail.com>
2023-05-06 18:57:49 +0200

    add start

--- src/exercises/2.5.1-store-actions/index.vue
+++ src/exercises/2.5.1-store-actions/index.vue
@@ -14,26 +14,5 @@ const dango = useDango()
   <button data-test="btn-stop-eating" :disabled="!dango.isEating" @click="dango.stopEating()">Stop eating</button>
   <button @click="dango.$reset()">Reset</button>
 
-  <!-- You won't need to change any of the classes of these elements -->
-  <section data-test="dangos" class="grid grid-cols-3 pt-8 pb-32">
-    <div class="">
-      <template v-if="dango.amount > 1">
-        <div v-for="i in dango.amount - 1" :key="i" class="h-[56px] -mb-[38px] animate__animated animate__bounceIn">
-          <DangoStick251 class="rotate-[262deg] translate-y-[-28px]" />
-        </div>
-      </template>
-    </div>
-
-    <div class="">
-      <div v-if="dango.amount > 0" :key="dango.amount" class="h-[56px] -mb-[38px] animate__animated animate__bounceIn">
-        <DangoStick251 :dango="3 - (dango.eatenBalls % 3)" />
-      </div>
-    </div>
-
-    <div class="">
-      <div v-for="i in dango.finishedSticks" :key="i" class="h-[56px] -mb-[46px] animate__animated animate__bounceIn">
-        <DangoStick251 :dango="0" class="rotate-[-262deg] translate-y-[-28px]" />
-      </div>
-    </div>
-  </section>
+  <EatingDango251 />
 </template>
