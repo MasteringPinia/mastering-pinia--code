b5543ed4542ef537dc7c7e5f7d087076a8f7aa42
Eduardo San Martin Morote <posva13@gmail.com>
2023-06-01 17:12:01 +0200

    fix license

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -42,6 +42,7 @@ declare module 'vue-router/auto/routes' {
     '/[...pathMatch]': RouteRecordInfo<'/[...pathMatch]', '/:pathMatch(.*)', { pathMatch: ParamValue<true> }, { pathMatch: ParamValue<false> }>,
     '/1.4-implementing-our-own-defineStore/': RouteRecordInfo<'/1.4-implementing-our-own-defineStore/', '/1/4-implementing-our-own-defineStore', Record<never, never>, Record<never, never>>,
     '/1.6-adding-inject-provide-to-defineStore/': RouteRecordInfo<'/1.6-adding-inject-provide-to-defineStore/', '/1/6-adding-inject-provide-to-defineStore', Record<never, never>, Record<never, never>>,
+    '/1.7-bonus-provide-inject-tab-system/': RouteRecordInfo<'/1.7-bonus-provide-inject-tab-system/', '/1/7-bonus-provide-inject-tab-system', Record<never, never>, Record<never, never>>,
     '/2.3.1-store-state/': RouteRecordInfo<'/2.3.1-store-state/', '/2/3/1-store-state', Record<never, never>, Record<never, never>>,
   }
 }
