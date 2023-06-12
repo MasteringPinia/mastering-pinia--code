f4ae4b24ddd6fe28d0f2cfc55d6e835aa9a414e5
Eduardo San Martin Morote <posva@users.noreply.github.com>
2023-06-12 16:10:38 +0200

    2.5.1 store actions (#6)

---
+++ src/exercises/2.5.1-store-actions/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { useDango } from './dango'
+
+const dango = useDango()
+</script>
+
+<template>
+  <h1>Let's eat some Dango 🍡</h1>
+
+  <button data-test="btn-eat" :disabled="dango.amount < 1 || dango.isEating" @click="dango.eatDango()">Eat!</button>
+  <button data-test="btn-start-eating" :disabled="dango.isEating || dango.amount < 1" @click="dango.startEating()">
+    Start Eating until finished
+  </button>
+  <button data-test="btn-stop-eating" :disabled="!dango.isEating" @click="dango.stopEating()">Stop eating</button>
+  <button @click="dango.$reset()">Reset</button>
+
+  <EatingDango251 />
+</template>
