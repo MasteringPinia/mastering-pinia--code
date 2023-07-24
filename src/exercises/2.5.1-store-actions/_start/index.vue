12427fea06e55838f9777903850bae51dfc9610d
Eduardo San Martin Morote <posva13@gmail.com>
2023-07-24 11:40:51 +0200

    space in code

--- src/exercises/2.5.1-store-actions/_start/index.vue
+++ src/exercises/2.5.1-store-actions/_start/index.vue
@@ -7,10 +7,12 @@ const dango = useDango()
 <template>
   <h1>Let's eat some Dango 🍡</h1>
 
-  <button data-test="btn-eat" :disabled="dango.amount < 1">Eat!</button>
-  <button data-test="btn-start-eating" :disabled="dango.amount < 1">Start Eating until finished</button>
-  <button data-test="btn-stop-eating">Stop eating</button>
-  <button @click="dango.$reset()">Reset</button>
+  <div class="space-x-2">
+    <button data-test="btn-eat" :disabled="dango.amount < 1">Eat!</button>
+    <button data-test="btn-start-eating" :disabled="dango.amount < 1">Start Eating until finished</button>
+    <button data-test="btn-stop-eating">Stop eating</button>
+    <button @click="dango.$reset()">Reset</button>
+  </div>
 
   <EatingDango251 />
 </template>
--- src/exercises/2.5.1-store-actions/index.vue
+++ src/exercises/2.5.1-store-actions/index.vue
@@ -7,12 +7,14 @@ const dango = useDango()
 <template>
   <h1>Let's eat some Dango 🍡</h1>
 
-  <button data-test="btn-eat" :disabled="dango.amount < 1 || dango.isEating" @click="dango.eatDango()">Eat!</button>
-  <button data-test="btn-start-eating" :disabled="dango.isEating || dango.amount < 1" @click="dango.startEating()">
-    Start Eating until finished
-  </button>
-  <button data-test="btn-stop-eating" :disabled="!dango.isEating" @click="dango.stopEating()">Stop eating</button>
-  <button @click="dango.$reset()">Reset</button>
+  <div class="space-x-2">
+    <button data-test="btn-eat" :disabled="dango.amount < 1 || dango.isEating" @click="dango.eatDango()">Eat!</button>
+    <button data-test="btn-start-eating" :disabled="dango.isEating || dango.amount < 1" @click="dango.startEating()">
+      Start Eating until finished
+    </button>
+    <button data-test="btn-stop-eating" :disabled="!dango.isEating" @click="dango.stopEating()">Stop eating</button>
+    <button @click="dango.$reset()">Reset</button>
+  </div>
 
   <EatingDango251 />
 </template>
