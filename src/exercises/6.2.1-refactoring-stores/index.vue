dc31fd716507a592b25c048a4a71a10a6e309230
Eduardo San Martin Morote <posva13@gmail.com>
2023-09-22 11:29:27 +0200

    wip ex

---
+++ src/exercises/6.2.1-refactoring-stores/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { useTasksStore } from './stores/tasks'
+import { useTodosStore } from './stores/todos'
+
+const todosStore = useTodosStore()
+const tasksStore = useTasksStore()
+</script>
+
+<template>
+  <h1>Task Management</h1>
+
+  <nav>
+    <RouterLink to="/6/2/1-refactoring-stores/some-test">Some Test</RouterLink>
+  </nav>
+
+  <pre>{{ todosStore.$state }}</pre>
+  <pre>{{ tasksStore.$state }}</pre>
+</template>
