e1531e46548ce96141217c4f006a9a49c3380b4c
Eduardo San Martin Morote <posva13@gmail.com>
2023-10-04 20:09:37 +0200

    remove incomplete ex

--- src/exercises/6.2.1-refactoring-stores/_start/index.vue
+++
@@ -1,8 +0,0 @@
-<script lang="ts" setup>
-// add variables here
-</script>
-
-<template>
-  <h1>base</h1>
-  <p>Copy the index.vue from the upper folder here and remove what you think makes sense to create a starting point.</p>
-</template>
