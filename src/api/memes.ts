9435ffd0180c19f368925bab45347753590917da
Eduardo San Martin Morote <posva13@gmail.com>
2023-10-04 10:45:18 +0200

    updates to memes

--- src/api/memes.ts
+++ src/api/memes.ts
@@ -17,10 +17,75 @@ export interface Meme {
   box_count: number
 }
 
+/**
+ * Get a list of memes.
+ */
 export function getMemes() {
   return imgflip
     .get<{
       data: { memes: Meme[] }
     }>('/get_memes')
     .then(({ data }) => data.memes)
+}
+
+/**
+ * Generate a meme on imgflip.
+ */
+export function captionImage({
+  id,
+  texts,
+  font,
+  maxFontSize,
+}: {
+  /**
+   * Template id of the meme
+   */
+  id: string
+
+  /**
+   * Text to put on the top of the meme
+   */
+  texts: string[]
+
+  font?: string
+
+  maxFontSize?: number
+}) {
+  const formData = new FormData()
+  formData.append('template_id', id)
+  if (!import.meta.env.VITE_IMGFLIP_USERNAME || !import.meta.env.VITE_IMGFLIP_PASSWORD) {
+    console.error('Set the VITE_IMGFLIP_USERNAME and VITE_IMGFLIP_PASSWORD in your .env file in order to use this API.')
+  }
+
+  formData.append('username', import.meta.env.VITE_IMGFLIP_USERNAME)
+  formData.append('password', import.meta.env.VITE_IMGFLIP_PASSWORD)
+  // formData.append('text0', texts[0])
+  // formData.append('text1', texts[1])
+  texts.forEach((text, i) => {
+    formData.set(`boxes[${i}][text]`, text)
+  })
+
+  return imgflip
+    .post<
+      | {
+          success: true
+          data: {
+            url: string
+            page_url: string
+          }
+        }
+      | {
+          success: false
+          error_message: string
+        }
+    >('/caption_image', formData, {
+      // body: formData,
+    })
+    .then(res => {
+      if (res.success) {
+        return res.data
+      } else {
+        throw new Error(res.error_message)
+      }
+    })
 }
--- src/exercises/6.2.1-refactoring-stores/pages/some-test.vue
+++ src/exercises/6.2.1-refactoring-stores/pages/some-test.vue
@@ -1,5 +1,134 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Meme, getMemes, captionImage } from '@/api/memes'
+import { ref } from 'vue'
+import { shallowRef } from 'vue'
+import { useFuse } from '@vueuse/integrations/useFuse'
+import FuseTextMatch from '@/components/.internal/FuseTextMatch.vue'
+
+const memes = shallowRef<Meme[]>([])
+const selectedMeme = shallowRef<Meme | null>(null)
+const texts = ref<string[]>([])
+
+getMemes().then(data => {
+  memes.value = data
+})
+
+const searchText = ref('')
+const { results } = useFuse(searchText, memes, {
+  fuseOptions: {
+    includeMatches: true,
+    keys: ['name'],
+  },
+  matchAllWhenSearchEmpty: true,
+})
+
+async function generateMeme() {
+  if (!selectedMeme.value) return
+
+  const meme = await captionImage({
+    texts: texts.value,
+    id: selectedMeme.value.id,
+  })
+
+  console.log(meme)
+}
+</script>
 
 <template>
   <p>Hello</p>
+
+  <section v-if="selectedMeme">
+    <p>Selected Meme: "{{ selectedMeme.name }}"</p>
+    <img style="width: 100px" :src="selectedMeme.url" :alt="selectedMeme.name" />
+
+    <ol>
+      <li v-for="i in selectedMeme.box_count">
+        <label>
+          Text {{ i }}
+          <input type="text" v-model="texts[i - 1]" />
+        </label>
+      </li>
+    </ol>
+
+    <button @click="generateMeme">Generate</button>
+  </section>
+
+  <p v-else>Select a meme to create your own</p>
+
+  <div>
+    <label>
+      Search:
+      <input type="text" v-model="searchText" />
+    </label>
+  </div>
+
+  <section class="masonry">
+    <figure
+      v-for="{ item: meme, matches } in results"
+      class="item"
+      :id="`${meme.name}_${meme.id}`"
+      :title="meme.name"
+      :key="meme.id"
+      @click="selectedMeme = meme"
+    >
+      <img class="item__content" :src="meme.url" :alt="meme.name" />
+      <figcaption>
+        <a :href="`#${meme.name}_${meme.id}`">
+          <FuseTextMatch v-if="matches?.[0]" :match="matches[0]"></FuseTextMatch>
+          <template v-else>{{ meme.name }}</template>
+        </a>
+      </figcaption>
+    </figure>
+  </section>
 </template>
+
+<style scoped>
+.masonry img {
+  max-width: 100%;
+  display: block;
+  margin-bottom: 0;
+}
+
+figure {
+  margin: 0;
+  display: grid;
+  grid-template-rows: 1fr auto;
+  margin-bottom: 10px;
+  break-inside: avoid;
+}
+
+figure > img {
+  grid-row: 1 / -1;
+  grid-column: 1;
+}
+
+figure a {
+  color: black;
+  text-decoration: none;
+}
+
+figcaption {
+  grid-row: 2;
+  grid-column: 1;
+  background-color: rgba(255, 255, 255, 0.5);
+  padding: 0.2em 0.5em;
+  justify-self: start;
+}
+
+.masonry {
+  column-count: 4;
+  column-gap: 10px;
+}
+
+@media screen and (max-width: 1024px) {
+  .masonry {
+    column-count: 3;
+  }
+}
+
+@media screen and (max-width: 500px) {
+  .masonry {
+    column-count: 2;
+  }
+}
+</style>
