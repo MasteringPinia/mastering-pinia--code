cd5f10fb6aab8110697390bf695267a10a37a319
Eduardo San Martin Morote <posva13@gmail.com>
2023-10-10 09:44:16 +0200

    minor change

--- src/views/index.vue
+++ src/views/index.vue
@@ -8,7 +8,10 @@ const showReviewWarning = import.meta.env.VITE_GIT_CLONE_WARNING !== 'off'
 <template>
   <h2 class="mb-6">Exercises:</h2>
 
-  <div v-if="showReviewWarning" class="rounded-md bg-red-200 dark:bg-red-800 border py-2 px-6 mb-12">
+  <div
+    v-if="showReviewWarning"
+    class="rounded-md bg-red-200 dark:bg-red-800 border dark:border-red-50 border-red-950 py-2 px-6 mb-12"
+  >
     <h3>😅 Oops!</h3>
     <img src="/oops.gif" alt="Oh, wow!" class="max-h-24" />
     <p class="m-0 text-lg">⚠️ It looks like you <i>cloned the repository directly</i>.</p>
