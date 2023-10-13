95ebed76f739460d80fe328c56f552bdadf1a90c
Eduardo San Martin Morote <posva13@gmail.com>
2023-10-13 21:46:52 +0200

    add tests

--- src/exercises/6.5-private-state/index.vue
+++ src/exercises/6.5-private-state/index.vue
@@ -9,30 +9,43 @@ const readonlyCounter = useReadonlyCounter()
 <template>
   <h2>Private counter</h2>
 
-  <div class="space-x-2">
+  <div class="space-x-2 mb-2">
     <button @click="privateCounter.increment()">Increment by 1</button>
     <button @click="privateCounter.increment(10)">Increment by 10</button>
   </div>
 
   <p>Double: {{ privateCounter.double }}</p>
 
-  <p>Counter store state:</p>
+  <p>Counter store state (should be empty):</p>
   <pre><code>{{ privateCounter.$state }}</code></pre>
 
   <hr />
 
   <h2>Readonly Counter</h2>
 
-  <div class="space-x-2">
+  <div class="space-x-2 mb-2">
     <button @click="readonlyCounter.increment()">Increment by 1</button>
     <button @click="readonlyCounter.increment(10)">Increment by 10</button>
-    <!-- NOTE: this one should be highlighted in red -->
-    <button @click="readonlyCounter.n++">Direct increment (fails)</button>
+    <button
+      @click="
+        /* @ts-expect-error: this shouldn't be allowed */
+        readonlyCounter.n++
+      "
+    >
+      Direct increment (fails)
+    </button>
   </div>
 
   <p>N: {{ readonlyCounter.n }}</p>
   <p>Double: {{ readonlyCounter.double }}</p>
 
-  <p>Counter store state:</p>
+  <p>Counter store state (should be empty):</p>
   <pre><code>{{ readonlyCounter.$state }}</code></pre>
+
+  <hr />
+
+  <p>
+    If you are done with these, you can try the harder version
+    <RouterLink to="/6/5-private-state/private-store">Private Stores</RouterLink> 👹.
+  </p>
 </template>
