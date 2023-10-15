7da57a0bdd1ea551c0fa6c46687cef4bfc1090ba
Eduardo San Martin Morote <posva13@gmail.com>
2023-10-15 09:51:36 +0200

    starters

--- src/exercises/6.5-private-state/_start/private-store.ts
+++ src/exercises/6.5-private-state/_start/private-store.ts
@@ -2,17 +2,8 @@
 // to work on the Type Safety part
 /* eslint @typescript-eslint/no-explicit-any:1 */
 
-import { SetupStoreDefinition, defineStore } from 'pinia'
+import { defineStore } from 'pinia'
 
-export function definePrivateStore<Id extends string, PrivateStore, SS>(
-  id: Id,
-  privateStateFn: () => PrivateStore,
-  setup: (privateState: ReturnType<SetupStoreDefinition<string, PrivateStore>>) => SS,
-) {
-  const usePrivateStore = defineStore(id + '_private', privateStateFn)
-
-  return defineStore(id, () => {
-    const privateStore = usePrivateStore()
-    return setup(privateStore)
-  })
+export function definePrivateStore(id: any, privateStateFn: any, setup: any) {
+  return defineStore(id, setup)
 }
