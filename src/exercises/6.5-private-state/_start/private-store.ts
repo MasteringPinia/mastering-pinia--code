6468708d59ed8e04872439554ab2cdf38dd97c84
Eduardo San Martin Morote <posva13@gmail.com>
2023-10-18 09:37:56 +0200

    update _start

--- src/exercises/6.5-private-state/_start/private-store.ts
+++ src/exercises/6.5-private-state/_start/private-store.ts
@@ -3,7 +3,21 @@
 /* eslint @typescript-eslint/no-explicit-any:1 */
 
 import { defineStore } from 'pinia'
+import { computed, reactive, ref } from 'vue'
 
 export function definePrivateStore(id: any, privateStoreSetup: any, setup: any) {
-  return defineStore(id, setup)
+  return defineStore(id, () => {
+    // FIXME: you will have to rewrite the whole function
+    const n = ref(0)
+    const privateStore = reactive({
+      n: 0,
+      double: computed(() => n.value * 2),
+
+      increment(amount = 1) {
+        n.value += amount
+      },
+    })
+
+    return setup(privateStore)
+  })
 }
