dc1e5e0d1ef85c3b02214d25bec96ad23dcb85ca
Eduardo San Martin Morote <posva13@gmail.com>
2023-10-19 15:48:24 +0200

    add tips

--- src/exercises/6.5.1-private-state/.internal/private-state.spec.ts
+++ src/exercises/6.5.1-private-state/.internal/private-state.spec.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect, beforeEach } from 'vitest'
 import { createPinia, getActivePinia, setActivePinia } from 'pinia'
 import { toRaw } from 'vue'
-import { isComputed } from '@tests/utils'
+import { isComputed, tipOnFail } from '@tests/utils'
 import { useAuthStore } from '../stores/auth'
 import { mockHttpRequests } from '@tests/mocks/server'
 
@@ -17,18 +17,26 @@ describe('Private state in stores', () => {
     expect(auth.$state).not.toHaveProperty('currentUser')
   })
 
+  it('creates a currentUser property in the pinia state', () => {
+    useAuthStore()
+
+    tipOnFail(() => {
+      expect(Object.keys(getActivePinia()!.state.value)).toHaveLength(2)
+    }, `Make sure to define 2 stores with **different ids**, one for the private state and one for the rest.`)
+
+    tipOnFail(() => {
+      expect(JSON.stringify(getActivePinia()?.state.value)).includes('"currentUser":null')
+    }, `Make sure to keep the "currentUser" property in the private state **without renaming it**.`)
+  })
+
   it('exposes the current user as a getter', async () => {
     const auth = useAuthStore()
 
     expect(auth.$state).not.toHaveProperty('currentUser')
     expect(auth).toHaveProperty('currentUser')
-    expect(isComputed(toRaw(auth).currentUser)).toBe(true)
-  })
-
-  it('creates a currentUser property in the pinia state', () => {
-    useAuthStore()
-
-    expect(JSON.stringify(getActivePinia()?.state.value)).includes('"currentUser":null')
+    tipOnFail(() => {
+      expect(isComputed(toRaw(auth).currentUser)).toBe(true)
+    }, `You are exposing "currentUser" as a writable property but it shouldn't be writable outside of the store. Make sure it is a **getter** from pinia's perspective.`)
   })
 
   it('can run login and logout actions', async () => {
