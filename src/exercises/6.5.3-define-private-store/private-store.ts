e734d4828e9226490ddb0a88833946d3cd1a87ec
Eduardo San Martin Morote <posva13@gmail.com>
2023-10-19 16:10:39 +0200

    split ex

---
+++ src/exercises/6.5.3-define-private-store/private-store.ts
@@ -0,0 +1,18 @@
+// NOTE: remove this line (or change the 1 into 0) if you don't want
+// to work on the Type Safety part
+/* eslint @typescript-eslint/no-explicit-any:1 */
+
+import { SetupStoreDefinition, defineStore } from 'pinia'
+
+export function definePrivateStore<Id extends string, PrivateStore, StoreSetup>(
+  id: Id,
+  privateStoreSetup: () => PrivateStore,
+  setup: (privateState: ReturnType<SetupStoreDefinition<string, PrivateStore>>) => StoreSetup,
+) {
+  const usePrivateStore = defineStore(id + '_private', privateStoreSetup)
+
+  return defineStore(id, () => {
+    const privateStore = usePrivateStore()
+    return setup(privateStore)
+  })
+}
