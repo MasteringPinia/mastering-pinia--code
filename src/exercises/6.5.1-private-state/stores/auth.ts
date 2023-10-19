aa9178ce1c4323d9aa97b9dbc0b8b87b7932b40e
Eduardo San Martin Morote <posva13@gmail.com>
2023-10-19 15:26:40 +0200

    split fist ex

---
+++ src/exercises/6.5.1-private-state/stores/auth.ts
@@ -0,0 +1,38 @@
+import { User, UserRegister, login as _login, registerUser } from '@/api/auth'
+import { defineStore, acceptHMRUpdate } from 'pinia'
+import { computed, ref } from 'vue'
+
+const useAuthPrivate = defineStore('auth-private', () => {
+  const currentUser = ref<User | null>(null)
+
+  return { currentUser }
+})
+
+export const useAuthStore = defineStore('auth', () => {
+  const privateState = useAuthPrivate()
+
+  function signup(userInfo: UserRegister): Promise<User> {
+    return registerUser(userInfo)
+  }
+
+  async function login(email: string, password: string) {
+    const user = await _login({ email, password })
+    privateState.currentUser = user
+  }
+
+  function logout() {
+    privateState.currentUser = null
+  }
+
+  return {
+    currentUser: computed(() => privateState.currentUser),
+    signup,
+    login,
+    logout,
+  }
+})
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useAuthStore, import.meta.hot))
+  import.meta.hot.accept(acceptHMRUpdate(useAuthPrivate, import.meta.hot))
+}
