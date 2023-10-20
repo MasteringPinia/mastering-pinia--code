044bac8c90f9f52beca58b209c07ba5da08d1a9e
Eduardo San Martin Morote <posva13@gmail.com>
2023-10-20 11:20:43 +0200

    wip

--- src/exercises/6.3-refactoring-stores/components/TodoItem.vue
+++ src/exercises/6.3-refactoring-stores/components/TodoItem.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-import { TodoItem } from '../../stores/todos'
+import { computed, ref } from 'vue'
+import { TodoItem } from '../stores/todos'
+import { useTasksStore } from '../stores/tasks'
 
 const props = defineProps<{
   todo: TodoItem
@@ -9,6 +10,8 @@ const props = defineProps<{
 const emit = defineEmits<{
   (e: 'update', todo: TodoItem): void
   (e: 'delete', todo: TodoItem): void
+  (e: 'start', todo: TodoItem): void
+  (e: 'pause', todo: TodoItem): void
 }>()
 
 // null when not editing, otherwise a copy of the todo
@@ -21,19 +24,29 @@ function saveTodo() {
   emit('update', todoCopy.value!)
   todoCopy.value = null
 }
+
+const tasks = useTasksStore()
+
+const isTaskStarted = computed(() => tasks.isTodoStarted(props.todo.id))
 </script>
 
 <template>
   <div>
-    <form v-if="todoCopy" class="space-x-2 mb-0" @submit.prevent="saveTodo">
+    <form v-if="todoCopy" class="mb-0 space-x-2" @submit.prevent="saveTodo">
       <input v-model="todoCopy.text" type="text" />
       <button @click="saveTodo">Save</button>
       <button type="button" @click="todoCopy = null">Cancel</button>
     </form>
-    <div v-else class="space-x-2 mb-0">
+    <div v-else class="mb-0 space-x-2">
       <span :class="{ 'line-through': todo.finished, 'text-gray': todo.finished }">{{ todo.text }}</span>
-      <button @click="startEdit">Edit</button>
-      <button @click="emit('delete', todo)">Delete</button>
+      <template v-if="isTaskStarted">
+        <button @click="emit('pause', todo)">Pause Task</button>
+      </template>
+      <template v-else>
+        <button @click="startEdit">Edit</button>
+        <button @click="emit('delete', todo)">Delete</button>
+        <button @click="emit('start', todo)">Start Task</button>
+      </template>
     </div>
   </div>
 </template>
