be8917a376e81f222a35bd7368e8115e52751c61
Eduardo San Martin Morote <posva13@gmail.com>
2023-10-24 14:46:26 +0200

    implement auth api

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,12 +1,32 @@
 import { mande } from 'mande'
+import Cookie from 'js-cookie'
+import { useLocalStorage } from '@vueuse/core'
 
 export const users = mande('http://localhost:7777/users', {})
 
 export interface User {
+  id: string
+  /**
+   * The email of the user. It is unique
+   */
   email: string
+
+  /**
+   * The name of the user
+   */
   displayName: string
+
+  /**
+   * The photo of the user
+   */
   photoURL: string
+
+  /**
+   * The password of the user because we don't have a real db.
+   */
+  password: string
 }
+
 interface UserAuth {
   email: string
   password: string
@@ -26,7 +46,7 @@ export async function registerUser(user: UserRegister) {
       })
     ).length > 0
   ) {
-    throw new Error('User already exists')
+    throw new Error(`User "${user.email}" already exists`)
   }
 
   return users.post<User>('/', {
@@ -35,17 +55,60 @@ export async function registerUser(user: UserRegister) {
   })
 }
 
-export async function login(user: UserAuth) {
-  const userList = await users.get<User[]>('/', {
+export const USER_KEY = 'mp_user'
+
+export async function login({ email, password }: UserAuth) {
+  const matchedUsers = await users.get<User[]>('/', {
     query: {
-      email: user.email,
-      password: user.password,
+      email,
+      // Don't do this in production
+      password,
     },
   })
 
-  if (!userList.length) {
-    throw new Error('Invalid credentials')
+  if (!matchedUsers.length) {
+    throw new Error('Invalid Credentials')
   }
 
-  return userList[0]
+  const user = matchedUsers.at(0)!
+
+  Cookie.set(USER_KEY, user.email)
+  localStorage.set(USER_KEY, {
+    id: user.id,
+    email: user.email,
+    displayName: user.displayName,
+    photoURL: user.photoURL,
+  })
+
+  return user
 }
+
+export function logout() {
+  Cookie.remove(USER_KEY)
+  localStorage.removeItem(USER_KEY)
+}
+
+/**
+ * Reactive current user.
+ *
+ * @returns the current user or null if there is no user
+ */
+export const useCurrentUser = () =>
+  useLocalStorage<Pick<User, 'displayName' | 'email' | 'id' | 'photoURL'>>(USER_KEY, null, {
+    serializer: {
+      read: v => {
+        try {
+          return JSON.parse(v)
+        } catch (_err) {
+          return null
+        }
+      },
+      write: v => {
+        try {
+          return JSON.stringify(v)
+        } catch (_err) {
+          return 'null'
+        }
+      },
+    },
+  })
