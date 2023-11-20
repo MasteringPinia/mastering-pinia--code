b46af691ffa0ff810f119cfb7547e88a0eb8ace0
Eduardo San Martin Morote <posva13@gmail.com>
2023-11-20 16:52:49 +0100

    delete more

--- src/exercises/_template/index.vue
+++ src/exercises/_template/index.vue
@@ -1,20 +1,7 @@
 <script lang="ts" setup>
 // add variables here
-import { registerUser } from '~/api/auth'
-
-async function register() {
-  const user = await registerUser({
-    email: 'hello@gmail.com',
-    password: 'wow',
-    displayName: 'Hello',
-  })
-
-  console.log(user)
-}
 </script>
 
 <template>
   <h1>Template to start an exercise</h1>
-
-  <button @click="register">Click</button>
 </template>
